3009aa674bf273e3ea424bff55b8a0f34e9dbbbe
RenzoRomero <romeropajuelo.renzo@outlook.com>
2018-11-13 22:58:11 -0500

    Deploy
    Deploy

--- controllers/product.js
+++ controllers/product.js
@@ -14,7 +14,7 @@ function getProduct (req, res) {
 }
 
 function getProducts (req, res) {
-  Product.find({"status":"D"}, (err, products) => {
+  Product.find({"status":"A"}, (err, products) => {
     if(err) return res.status(500).send({message: `Error al realizar la peticion: ${err}`})
     if(!products) return res.status(404).send({message: `No existen productos`})
 
